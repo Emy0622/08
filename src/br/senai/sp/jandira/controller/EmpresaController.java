5b97a19d5eb98d51f7fd892427d580a20b0a73de
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2023-11-08 17:24:26 -0300

    fim

--- src/br/senai/sp/jandira/controller/EmpresaController.java
+++ src/br/senai/sp/jandira/controller/EmpresaController.java
@@ -1,4 +1,42 @@
 package br.senai.sp.jandira.controller;
 
+import br.senai.sp.jandira.model.Conexao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class EmpresaController {
+    public void consultarFuncionarios () throws SQLException {
+
+        //Representa a conexão
+        Conexao conexao = new Conexao();
+
+        // Responsavel por executar query
+        Connection objConnection = conexao.getConnection();
+
+        // Query
+        Statement statement = objConnection.createStatement();
+
+        //Armazena resposta do banco
+        String queryConsulta = "SELECT * FROM funcionarios";
+       ResultSet resultSet = statement.executeQuery(queryConsulta);
+
+        while (resultSet.next()){
+            int idFuncionario = resultSet.getInt("id_funcionario");
+            String nome = resultSet.getString("nome");
+            String cargo = resultSet.getString("cargo");
+            Double salario = resultSet.getDouble("salario");
+            String departamento = resultSet.getString("departamento");
+
+            System.out.println(idFuncionario);
+            System.out.println(nome);
+            System.out.println(cargo);
+            System.out.println(salario);
+            System.out.println(departamento);
+            System.out.println("_-_-_-_-_-_-_-_-_-");
+
+       }
+    }
 }
